33778cb8045172f3f4a6881fadc2edbed61c334c
Michael Francis <mfrancis107@gmail.com>
2024-04-01 20:38:45 -0400

    add drizzle kit

--- vite.config.ts
+++ vite.config.ts
@@ -11,4 +11,14 @@ export default defineConfig({
       "@": path.resolve(__dirname, "./src"),
     },
   },
+  server: {
+    host: true,
+    proxy: {
+      "/api": {
+        target: "http://127.0.0.1:3000/",
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ""),
+      },
+    },
+  },
 });
