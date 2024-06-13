a08cdec3aafacd32a3261f2c8d8b45c930f83f0e
Michael Francis <mfrancis107@gmail.com>
2024-06-13 09:27:19 -0400

    Server should references generated routes

--- src/server/index.ts
+++ src/server/index.ts
@@ -3,7 +3,6 @@ import { Hono } from "hono";
 import { cors } from "hono/cors";
 import { examplesRoute } from "./routes/examples";
 import { apiRoutes } from "./routes";
-
 const app = new Hono();
 app.use(
   "/*",
@@ -11,15 +10,11 @@ app.use(
     origin: "*",
   })
 );
-
 const port = 3000;
 console.log(`Server is running on port http://localhost:${port}`);
-
-const routes = app.route("/", apiRoutes).route("/examples", examplesRoute);
-
+const routes = app.route("/examples", examplesRoute).route("/", apiRoutes);
 serve({
   fetch: app.fetch,
   port,
 });
-
 export type AppType = typeof routes;
