6812d503c0550fc8adee9f9665ee15360796a54f
Michael Francis <mfrancis107@gmail.com>
2024-07-03 10:17:37 -0400

    prettier

--- src/server/middleware/index.ts
+++ src/server/middleware/index.ts
@@ -1,6 +1,5 @@
 import { Context, Next } from "hono";
 import { createMiddleware } from "hono/factory";
-
 export const authenticated = createMiddleware((c: Context, next: Next) => {
   return next();
 });
