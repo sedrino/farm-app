75e8fdba291a6b7addce4d414ac7e6c18af8b5e9
Michael Francis <mfrancis107@gmail.com>
2024-08-11 12:50:31 -0400

    add @std/ulid

--- src/utils/route-type.ts
+++ src/utils/route-type.ts
@@ -13,78 +13,69 @@ type RouteNode = {
   children?: Record<string, TempRoute> | unknown;
 };
 type RootRoute = typeof routeTree;
-type GetChildren<T extends TempRoute> =
-  T["types"]["children"] extends Record<string, TempRoute>
-    ? T["types"]["children"]
-    : never;
+type GetChildren<T extends TempRoute> = T["types"]["children"] extends
+  Record<string, TempRoute> ? T["types"]["children"]
+  : never;
 type RootChildren = GetChildren<RootRoute>;
 type t = GetChildren<RootChildren["IndexRoute"]>;
 type FlattenRoutes<T extends TempRoute> =
   | T["types"]
-  | (GetChildren<T> extends never
-      ? never
-      : {
-          [K in keyof GetChildren<T>]: FlattenRoutes<GetChildren<T>[K]>;
-        }[keyof GetChildren<T>]);
+  | (GetChildren<T> extends never ? never
+    : {
+      [K in keyof GetChildren<T>]: FlattenRoutes<GetChildren<T>[K]>;
+    }[keyof GetChildren<T>]);
 type AllRoutes = FlattenRoutes<RootRoute>;
 type ExtractTo<T> = T extends {
   to: infer R;
-}
-  ? R
+} ? R
   : never;
 type ExtractSearchSchemaInput<T, U> = T extends {
   to: U;
   searchSchemaInput: infer S;
-}
-  ? S
+} ? S
   : never;
 type ExtractSearchSchemaOutput<T, U> = T extends {
   to: U;
   searchSchema: infer S;
-}
-  ? S
+} ? S
   : never;
 type ExtractSearchSchema<T, U> = T extends {
   to: U;
   searchSchema: infer S;
-}
-  ? S
+} ? S
   : never;
 type ExtractFullSearchSchema<T, U> = T extends {
   to: U;
   fullSearchSchema: infer S;
-}
-  ? S
+} ? S
   : never;
 type ExtractAllParams<T, U> = T extends {
   to: U;
   allParams: infer S;
-}
-  ? S
+} ? S
   : never;
 type ExtractFullSearchSchemaInput<T, U> = T extends {
   to: U;
   fullSearchSchemaInput: infer S;
-}
-  ? S
+} ? S
   : never;
 type RouteKeys = ExtractTo<AllRoutes>;
 // expands object types recursively
-type ExpandRecursively<T> = T extends object
-  ? T extends infer O
-    ? {
-        [K in keyof O]: ExpandRecursively<O[K]>;
-      }
-    : never
+type ExpandRecursively<T> = T extends object ? T extends infer O ? {
+      [K in keyof O]: ExpandRecursively<O[K]>;
+    }
+  : never
   : T;
-export type RouteInfo = ExpandRecursively<{
-  [K in RouteKeys]: {
-    allParams: ExtractAllParams<AllRoutes, K>;
-    fullSearchSchema: ExtractFullSearchSchema<AllRoutes, K>;
-    fullSearchSchemaInput: ExtractFullSearchSchemaInput<AllRoutes, K>;
-    searchSchema: ExtractSearchSchema<AllRoutes, K>;
-    searchSchemaInput: ExtractSearchSchemaInput<AllRoutes, K>;
-    searchSchemaOutput: ExtractSearchSchemaOutput<AllRoutes, K>;
-    fullPath: K;
-  };
-}>;
+export type RouteInfo = ExpandRecursively<
+  {
+    [K in RouteKeys]: {
+      allParams: ExtractAllParams<AllRoutes, K>;
+      fullSearchSchema: ExtractFullSearchSchema<AllRoutes, K>;
+      fullSearchSchemaInput: ExtractFullSearchSchemaInput<AllRoutes, K>;
+      searchSchema: ExtractSearchSchema<AllRoutes, K>;
+      searchSchemaInput: ExtractSearchSchemaInput<AllRoutes, K>;
+      searchSchemaOutput: ExtractSearchSchemaOutput<AllRoutes, K>;
+      fullPath: K;
+    };
+  }
+>;
